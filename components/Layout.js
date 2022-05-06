360ca547299ef8a58077c55081bdef4c1468f191
Fahmi Akbar <fahmialiasakbar@gmail.com>
2022-05-06 19:18:38 +0700

    Creating Layout and Nav

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import Nav from './Nav'
+import styles from '../styles/Layout.module.css'
+
+const Layout = ({children}) => {
+    return (
+        <>
+            <Nav/>
+            <div className={styles.container}>
+                <div className={styles.main}>
+                    {children}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Layout
